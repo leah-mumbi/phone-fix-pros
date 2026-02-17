fd925665eac6e0e06b20cb4f4c6bf7c850a26972
Leah Mumbi <mumbileah245@gmail.com>
2026-02-17 22:44:54 +0300

    fixed image path

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -8,7 +8,7 @@ import { useCart } from "@/hooks/useCart";
 import screens from "@/assets/screens.png";
 import battery from "@/assets/battery.jpg";
 import screenProtectors from "@/assets/screen-protectors.png";
-import uv from "@/assets/uv.jpg";
+import uv from "../assets/uv.jpg"
 import phoneCase from "@/assets/phone-case.png";
 import iphoneCase from "@/assets/iphone-case.png";
 import accesories from "@/assets/accesories.png";
