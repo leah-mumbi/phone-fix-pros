dda4cbe4503bf17aadc81008fbacab6511c6ba18
Leah Mumbi <mumbileah245@gmail.com>
2026-02-19 23:23:46 +0300

    update file

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App = () => (
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </BrowserRouter>
